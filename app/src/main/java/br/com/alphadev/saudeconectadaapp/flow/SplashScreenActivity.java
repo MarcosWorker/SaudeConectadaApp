505e2b57ccc1a308f308e79a81fac05a515dd2ec
MarcosWorker <marcos_worker@outlook.com>
2017-09-13 16:01:52 -0300

    implementação do designer das telas

--- app/src/main/java/br/com/alphadev/saudeconectadaapp/flow/SplashScreenActivity.java
+++ app/src/main/java/br/com/alphadev/saudeconectadaapp/flow/SplashScreenActivity.java
@@ -17,7 +17,7 @@ public class SplashScreenActivity extends Activity {
             @Override
             public void run() {
                 try {
-                    sleep(2000);
+                    sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
